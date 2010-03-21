cfc5f6e6f2ecc468fa371c06d1f4165848e43295
Yann PELUD <yann@MacBook.local>
2010-03-21 16:13:41 +0100

    ajout des images captcha

--- config/initializers/load_settings.rb
+++ config/initializers/load_settings.rb
@@ -20,3 +20,5 @@
 #
 
 APP_CONFIG = YAML::load_file(File.join(RAILS_ROOT, "config", "settings.yml"))[RAILS_ENV]
+ENV['RECAPTCHA_PUBLIC_KEY']  = APP_CONFIG['captcha_pub_key']
+ENV['RECAPTCHA_PRIVATE_KEY'] = APP_CONFIG['captcha_pri_key']
