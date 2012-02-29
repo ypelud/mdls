213b701c3896c5a5ce061ea9460361b8a33e8021
ypelud <ypelud@gmail.com>
2012-02-29 07:18:24 +0100

    Renommage willl en will

---
+++ config/initializers/will_paginate.rb
@@ -0,0 +1,4 @@
+# paramètre pagination 
+
+WillPaginate::ViewHelpers.pagination_options[:previous_label] = '<<'
+WillPaginate::ViewHelpers.pagination_options[:next_label] = '>>'
--- config/initializers/willl_paginate.rb
+++
@@ -1,4 +0,0 @@
-# paramètre pagination 
-
-WillPaginate::ViewHelpers.pagination_options[:previous_label] = '◄'
-WillPaginate::ViewHelpers.pagination_options[:next_label] = '►'
