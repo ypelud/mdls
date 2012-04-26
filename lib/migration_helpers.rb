d07aeaec5d06d410a562e3ca4a6943573d933672
ypelud <ypelud@gmail.com>
2012-04-26 21:54:12 +0200

    gestion de la clé étrangère lors de la migration pour sqlite

--- lib/migration_helpers.rb
+++ lib/migration_helpers.rb
@@ -1,10 +1,10 @@
 module MigrationHelpers
   def foreign_key(from_table, from_column, to_table, to_column)
-    constraint_name = "fk_#{from_table}_#{from_column}" 
-
-    execute %{alter table #{from_table}
-              add constraint #{constraint_name}
-              foreign key (#{from_column})
-              references #{to_table}(#{to_column})}
+    adapter_type = adapter_name.downcase.to_sym
+  	unless adapter_type == :sqlite then 
+  	  constraint_name = "fk_#{from_table}_#{from_column}" 
+      execute %{alter table #{from_table} add constraint #{constraint_name}
+              foreign key (#{from_column}) references #{to_table}(#{to_column})}
+    end
   end
 end
